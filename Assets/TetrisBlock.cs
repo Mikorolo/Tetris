c792ed6a270573f0c67fd52811fdf6d95ac8511b
Mikorolo <mikolaj.widanka@gmail.com>
2021-11-04 23:03:09 +0100

    Added tetrominos and basic logic

---
+++ Assets/TetrisBlock.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    private float previousTime;
+    public Vector3 rotationPoint;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(Time.time - previousTime > fallTime / 10)
+            {
+                transform.position += new Vector3(-1, 0, 0);
+                if (!checkMove())
+                {
+                    transform.position -= new Vector3(-1, 0, 0);
+                }
+                previousTime = Time.time;
+            }
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if (Time.time - previousTime > fallTime / 10)
+            {
+                transform.position += new Vector3(1, 0, 0);
+                if (!checkMove())
+                {
+                    transform.position -= new Vector3(1, 0, 0);
+                }
+                previousTime = Time.time;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!checkMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if(!checkMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+        }
+
+        if(Time.time - previousTime > 
+            (Input.GetKey(KeyCode.DownArrow)
+            ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            previousTime = Time.time;
+            if (!checkMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    bool checkMove()
+    {
+        foreach (Transform blocks in transform)
+        {
+            int borderX = Mathf.RoundToInt(blocks.transform.position.x);
+            int borderY = Mathf.RoundToInt(blocks.transform.position.y);
+            if (borderX >= width || borderX < 0 || borderY >= height || borderY < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
